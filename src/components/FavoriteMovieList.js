f5c6c4013ae3313a5e2b3d628103f90ab67be447
Winifred Lu <winnie.lu@comcast.net>
2022-01-18 15:59:00 -0500

    progress on favorite functionality, break time

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeFavorite } from '../actions/favoritesActions';
 
 
 const FavoriteMovieList = (props) => {
     const favorites = props.favorites;
+    const handleRemove = (id) => {
+        props.removeFavorite(id);
+    }
     
     return (<div className="col-xs savedContainer">
         <h5>Favorite Movies</h5>
@@ -13,7 +17,7 @@ const FavoriteMovieList = (props) => {
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
+                        <span><span onClick={() => handleRemove(movie.id)} className="material-icons">remove_circle</span></span>
                     </Link> 
                 </div>
             })
@@ -27,4 +31,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(FavoriteMovieList);
+export default connect(mapStateToProps, { removeFavorite })(FavoriteMovieList);
--- src/reducers/favoriteReducer.js
+++ src/reducers/favoriteReducer.js
@@ -7,6 +7,23 @@ const initialState = {
 
 const reducer = (state=initialState, action) => {
     switch(action.type) {
+        case TOGGLE_FAVORITES:
+            return {
+                ...state,
+                displayFavorites: !state.displayFavorites
+            }
+        case ADD_FAVORITE:
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        case REMOVE_FAVORITE:
+            return {
+                ...state,
+                favorites: state.favorites.filter(movie => {
+                    movie.id != action.payload
+                })
+            }
         default:
             return state;
     }
