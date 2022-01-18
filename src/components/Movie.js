b8041e667edc538edb9e7756a595815f2fbcd5da
Winifred Lu <winnie.lu@comcast.net>
2022-01-18 15:29:09 -0500

    reducer refactoring reconnected

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -54,7 +54,7 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        movies: state.movies
+        movies: state.movie.movies
     }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 
+import favoriteReducer from './favoriteReducer';
 import movieReducer from './movieReducer';
 
-export default movieReducer;
+export default combineReducers({
+    movie: movieReducer,
+    favorite: favoriteReducer,
+});
